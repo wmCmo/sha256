b9a37a1f7d417f3f345f1ac88443598dbcb22124
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-04-28 22:22:52 +0900

    Fix preprocess function

--- sha256.py
+++ sha256.py
@@ -5,21 +5,11 @@ def bin_string(inpt):
     return outpt
 
 
-#preprocessing
 def preprocess(inpt):
     bin_str = bin_string(inpt)
     bin_length = len(bin_str)
-    zeroes = bin_length%512 if bin_length > 512 else 512%bin_length
 
-    bin_str += '1'
+    zeroes = 512 - ((bin_length+64+1) % 512)
+    l_zeroes = 64 - len(bin(bin_length)[2:])
 
-    l_zero = 64 - len(bin(bin_length))
-    
-    
-    return bin_str + '1' + ('0'*zeroes) + (l_zero * '0') + str(bin(bin_length)[2:])
-
-
-
-
-
-print(preprocess('Hello'))
+    return bin_str + '1' + ('0'*zeroes) + ('0' * l_zeroes) + str(bin(bin_length)[2:])
