aa37e1cf765dfbe02178db7b6f2a38a2a27065ca
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-04-28 22:56:30 +0900

    Parsing function completed

--- sha256.py
+++ sha256.py
@@ -13,3 +13,18 @@ def preprocess(inpt):
     l_zeroes = 64 - len(bin(bin_length)[2:])
 
     return bin_str + '1' + ('0'*zeroes) + ('0' * l_zeroes) + str(bin(bin_length)[2:])
+
+
+def parsing(inpt):
+    padded = preprocess(inpt)
+    N = len(padded)//512
+
+    parsed = []
+
+    for block in range(N):
+        parsed.append([])
+        block_i = padded[512*block:512*(block+1)]
+        for bit_word in range(int(len(block_i)/32)):
+            parsed[block].append([])
+            parsed[block][bit_word] = block_i[32*bit_word:32*(bit_word+1)]
+    return parsed
